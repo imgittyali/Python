1f73461d4dc0697ca4bf09a1d937a5ae3d5fa80d
Eljaouhari, Abdelali <eljaouhariabdelali@gmail.com>
2018-06-08 18:26:13 -0600

    add new file

---
+++ SentDex.py
@@ -0,0 +1,14 @@
+#   I just want to write something to look back at for each lesson.  
+#   Int function works with whole numbers only EX: Print(int(8+3.3)) will output 11 only. 
+#   Print(float(8+3.3)) , however, will output the correct amount of 11.3 
+#   Incasing anything in single or double quotes makes the content inside a string. 
+#   we\'re going to use a \ whenever '' is used. Concatentation happens 
+#   when two strings are added to one another. example: Hi + there will result in
+#   hithere.
+
+
+print('This is me taking python seriously')
+print('So.. In order to concatenate, a comma is needed?')
+print('How bout a high 5?')
+print(int(8+3.3))
+
